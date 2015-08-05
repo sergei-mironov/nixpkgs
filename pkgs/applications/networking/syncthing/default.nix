b8baef9d991d66fc92998fb41413328a6c8396b8
Sergey Mironov <grrwlf@gmail.com>
2015-08-05 21:59:05 +0000

    syncthing: jump to 11.16

--- pkgs/applications/networking/syncthing/default.nix
+++ pkgs/applications/networking/syncthing/default.nix
@@ -4,12 +4,12 @@ with goPackages;
 
 buildGoPackage rec {
   name = "syncthing-${version}";
-  version = "0.11.1";
+  version = "0.11.16";
   goPackagePath = "github.com/syncthing/syncthing";
   src = fetchgit {
     url = "git://github.com/syncthing/syncthing.git";
     rev = "refs/tags/v${version}";
-    sha256 = "518add39e2239fc8575cdf5cafc3562f006df7201fbd272077ed3bbbbfd816d4";
+    sha256 = "f9b5c2de7e2b6592cccb0222c48b9baa2497dce519824a75923d40cc722ab937";
   };
 
   subPackages = [ "cmd/syncthing" ];
