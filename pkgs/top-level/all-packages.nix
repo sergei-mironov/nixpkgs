c5680d92fe8e0cd843891c9092de57764cfe1ae0
Sergey Mironov <grrwlf@gmail.com>
2015-12-09 01:25:46 +0300

    sox: read enableLame from the config

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12966,7 +12966,9 @@ let
     inherit (pkgs.vamp) vampSDK;
   };
 
-  sox = callPackage ../applications/misc/audio/sox { };
+  sox = callPackage ../applications/misc/audio/sox {
+    enableLame = config.sox.enableLame or false;
+  };
 
   soxr = callPackage ../applications/misc/audio/soxr { };
 
