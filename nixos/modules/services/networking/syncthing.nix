5ce950458bbc054248a3f48630560792a061ab33
Sergey Mironov <grrwlf@gmail.com>
2017-01-11 12:09:29 +0300

    syncthing: turn back all_proxy logic

--- nixos/modules/services/networking/syncthing.nix
+++ nixos/modules/services/networking/syncthing.nix
@@ -13,8 +13,9 @@ let
     environment = {
       STNORESTART = "yes";
       STNOUPGRADE = "yes";
-      inherit (cfg) all_proxy;
-    } // config.networking.proxy.envVars;
+    } // config.networking.proxy.envVars
+      # We allow overwriting global all_proxy var with the syncthing.all_proxy
+      // (if cfg.all_proxy != "" then { inherit (cfg) all_proxy; } else {});
   };
 
   service = {
