20d31847e016e08496911da62c68997d80741bcd
Sergey Mironov <grrwlf@gmail.com>
2017-04-25 16:17:45 +0300

    geth: 1.6.0

--- pkgs/applications/altcoins/ethereum/geth.nix
+++ pkgs/applications/altcoins/ethereum/geth.nix
@@ -8,8 +8,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "go-ethereum";
-    rev = "8938768f75c6a3c5a0bfb8ae0fed43b7d9fb7e7a";
-    sha256 = "07dx19dg5w3qg11z860v74qkbmm9i1zb42az7xhkh763gq62lj0a";
+    rev = "facc47cb5cec97b22c815a0a6118816a98f39876";
+    sha256 = "0zk3lrzb3lckj8ffk0mj4kmgrzviq70xnkhrzx01r1nxlk648g02";
   };
 
   installPhase = ''
