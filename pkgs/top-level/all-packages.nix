45ba50e8749b8ee11b98bbf5443cffe98fb0093e
Sergey Mironov <grrwlf@gmail.com>
2017-07-14 15:39:29 +0300

    backport libtool242

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9727,6 +9727,7 @@ in
   libtool_1_5 = callPackage ../development/tools/misc/libtool { };
 
   libtool_2 = callPackage ../development/tools/misc/libtool/libtool2.nix { };
+  libtool_2_4_2 = callPackage ../development/tools/misc/libtool/libtool242.nix { };
 
   libwhich = callPackage ../development/tools/misc/libwhich { };
 
