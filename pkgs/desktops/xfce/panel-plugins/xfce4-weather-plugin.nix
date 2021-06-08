73403c65ba64e9bfb32d05b330481055b91fe7a7
Sergey Mironov <grrwlf@gmail.com>
2021-06-08 22:16:42 +0300

    [FIX] xfce4-weather-plugin 0.8 -> 0.11

--- pkgs/desktops/xfce/panel-plugins/xfce4-weather-plugin.nix
+++ pkgs/desktops/xfce/panel-plugins/xfce4-weather-plugin.nix
@@ -1,21 +1,21 @@
-{ stdenv, fetchurl, pkgconfig, intltool, gtk2, libxml2, libsoup, upower,
-libxfce4ui, libxfce4util, xfce4-panel, hicolor-icon-theme }:
+{ stdenv, fetchurl, pkgconfig, intltool, gtk3, libxml2, libsoup, upower,
+libxfce4ui, libxfce4util, xfce4-panel, hicolor-icon-theme, xfconf }:
 
 stdenv.mkDerivation rec {
   name = "${p_name}-${ver_maj}.${ver_min}";
   p_name  = "xfce4-weather-plugin";
-  ver_maj = "0.8";
-  ver_min = "10";
+  ver_maj = "0.11";
+  ver_min = "0";
 
   src = fetchurl {
     url = "mirror://xfce/src/panel-plugins/${p_name}/${ver_maj}/${name}.tar.bz2";
-    sha256 = "1f7ac2zr5s5w6krdpgsq252wxhhmcblia3j783132ilh8k246vgf";
+    sha256 = "sha256:1z2k24d599mxf5gqa35i3xmc3gk2yvqs80hxxpyw06yma6ljw973";
   };
 
   nativeBuildInputs = [ pkgconfig intltool ];
 
-  buildInputs = [ gtk2 libxml2 libsoup upower libxfce4ui libxfce4util
-   xfce4-panel hicolor-icon-theme ];
+  buildInputs = [ gtk3 libxml2 libsoup upower libxfce4ui libxfce4util
+   xfce4-panel hicolor-icon-theme xfconf ];
 
   enableParallelBuilding = true;
 
