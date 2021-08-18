a286260ad14d6ac765ef36182aa321c39872e4ff
Sergey Mironov <grrwlf@gmail.com>
2021-08-18 12:10:21 +0300

    kitty: backporting 0.21.2 from master

--- pkgs/applications/misc/kitty/default.nix
+++ pkgs/applications/misc/kitty/default.nix
@@ -1,8 +1,10 @@
-{ stdenv, substituteAll, fetchFromGitHub, python3Packages, libunistring,
-  harfbuzz, fontconfig, pkgconfig, ncurses, imagemagick, xsel,
+{ lib, stdenv, fetchFromGitHub, python3Packages, libunistring,
+  harfbuzz, fontconfig, pkg-config, ncurses, imagemagick, xsel,
   libstartup_notification, libGL, libX11, libXrandr, libXinerama, libXcursor,
   libxkbcommon, libXi, libXext, wayland-protocols, wayland,
-  which, dbus,
+  lcms2,
+  installShellFiles,
+  dbus,
   Cocoa,
   CoreGraphics,
   Foundation,
@@ -12,8 +14,6 @@
   libcanberra,
   libicns,
   libpng,
-  librsvg,
-  optipng,
   python3,
   zlib,
 }:
@@ -21,20 +21,21 @@
 with python3Packages;
 buildPythonApplication rec {
   pname = "kitty";
-  version = "0.16.0";
+  version = "0.21.2";
   format = "other";
 
   src = fetchFromGitHub {
     owner = "kovidgoyal";
     repo = "kitty";
     rev = "v${version}";
-    sha256 = "1bszyddar0g1gdz67h8rd3gbrdhi6ahjg7j14cjiqxm1938z9ajf";
+    sha256 = "0y0mg8rr18mn0wzym7v48x6kl0ixd5q387kr5jhbdln55ph2jk9d";
   };
 
   buildInputs = [
     harfbuzz
     ncurses
-  ] ++ stdenv.lib.optionals stdenv.isDarwin [
+    lcms2
+  ] ++ lib.optionals stdenv.isDarwin [
     Cocoa
     CoreGraphics
     Foundation
@@ -44,45 +45,57 @@ buildPythonApplication rec {
     libpng
     python3
     zlib
-  ] ++ stdenv.lib.optionals stdenv.isLinux [
+  ] ++ lib.optionals stdenv.isLinux [
     fontconfig libunistring libcanberra libX11
     libXrandr libXinerama libXcursor libxkbcommon libXi libXext
     wayland-protocols wayland dbus
   ];
 
   nativeBuildInputs = [
-    pkgconfig which sphinx ncurses
-  ] ++ stdenv.lib.optionals stdenv.isDarwin [
+    pkg-config sphinx ncurses
+    installShellFiles
+  ] ++ lib.optionals stdenv.isDarwin [
     imagemagick
     libicns  # For the png2icns tool.
-    librsvg
-    optipng
   ];
 
-  propagatedBuildInputs = stdenv.lib.optional stdenv.isLinux libGL;
+  propagatedBuildInputs = lib.optional stdenv.isLinux libGL;
 
   outputs = [ "out" "terminfo" ];
 
-  patches = [
-    ./fix-paths.patch
-  ] ++ stdenv.lib.optionals stdenv.isLinux [
-    (substituteAll {
-      src = ./library-paths.patch;
-      libstartup_notification = "${libstartup_notification}/lib/libstartup-notification-1.so";
-      libcanberra = "${libcanberra}/lib/libcanberra.so";
-      libEGL = "${stdenv.lib.getLib libGL}/lib/libEGL.so.1";
-    })
-  ] ++ stdenv.lib.optionals stdenv.isDarwin [
-    ./no-lto.patch
-    ./no-werror.patch
-    ./png2icns.patch
-  ];
+  # Causes build failure due to warning
+  hardeningDisable = lib.optional stdenv.cc.isClang "strictoverflow";
 
-  buildPhase = if stdenv.isDarwin then ''
-    ${python.interpreter} setup.py kitty.app --update-check-interval=0
-  '' else ''
-    ${python.interpreter} setup.py linux-package --update-check-interval=0
+  dontConfigure = true;
+
+  buildPhase = ''
+    runHook preBuild
+    ${if stdenv.isDarwin then ''
+      ${python.interpreter} setup.py kitty.app \
+      --update-check-interval=0 \
+      --disable-link-time-optimization
+      make man
+    '' else ''
+      ${python.interpreter} setup.py linux-package \
+      --update-check-interval=0 \
+      --egl-library='${lib.getLib libGL}/lib/libEGL.so.1' \
+      --startup-notification-library='${libstartup_notification}/lib/libstartup-notification-1.so' \
+      --canberra-library='${libcanberra}/lib/libcanberra.so'
+    ''}
+    runHook postBuild
   '';
+
+  checkInputs = [ pillow ];
+
+  checkPhase =
+    let buildBinPath =
+      if stdenv.isDarwin
+        then "kitty.app/Contents/MacOS"
+        else "linux-package/bin";
+    in
+    ''
+      env PATH="${buildBinPath}:$PATH" ${python.interpreter} test.py
+    '';
 
   installPhase = ''
     runHook preInstall
@@ -92,10 +105,12 @@ buildPythonApplication rec {
     ln -s ../Applications/kitty.app/Contents/MacOS/kitty "$out/bin/kitty"
     mkdir "$out/Applications"
     cp -r kitty.app "$out/Applications/kitty.app"
+
+    installManPage 'docs/_build/man/kitty.1'
     '' else ''
     cp -r linux-package/{bin,share,lib} $out
     ''}
-    wrapProgram "$out/bin/kitty" --prefix PATH : "$out/bin:${stdenv.lib.makeBinPath [ imagemagick xsel ncurses.dev ]}"
+    wrapProgram "$out/bin/kitty" --prefix PATH : "$out/bin:${lib.makeBinPath [ imagemagick xsel ncurses.dev ]}"
     runHook postInstall
 
     # ZSH completions need to be invoked with `source`:
@@ -118,10 +133,11 @@ buildPythonApplication rec {
     echo "$terminfo" >> $out/nix-support/propagated-user-env-packages
   '';
 
-  meta = with stdenv.lib; {
-    homepage = https://github.com/kovidgoyal/kitty;
+  meta = with lib; {
+    homepage = "https://github.com/kovidgoyal/kitty";
     description = "A modern, hackable, featureful, OpenGL based terminal emulator";
     license = licenses.gpl3;
+    changelog = "https://sw.kovidgoyal.net/kitty/changelog.html";
     platforms = platforms.darwin ++ platforms.linux;
     maintainers = with maintainers; [ tex rvolosatovs ma27 ];
   };
