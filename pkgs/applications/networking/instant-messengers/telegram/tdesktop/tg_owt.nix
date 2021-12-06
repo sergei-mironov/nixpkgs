0a4fea66d5ef8da37506877d47b05336c29b071c
Sergey Mironov <grrwlf@gmail.com>
2021-12-06 14:37:19 +0300

    Revert "tdesktop: 2.7.5 -> 2.8.0"
    This reverts commit d73ddfe782358fc3507b5153cc09874503299d89.

--- pkgs/applications/networking/instant-messengers/telegram/tdesktop/tg_owt.nix
+++ pkgs/applications/networking/instant-messengers/telegram/tdesktop/tg_owt.nix
@@ -1,33 +1,22 @@
-{ lib, stdenv, fetchFromGitHub, fetchpatch
-, pkg-config, cmake, ninja, yasm
+{ lib, stdenv, fetchFromGitHub, pkg-config, cmake, ninja, yasm
 , libjpeg, openssl, libopus, ffmpeg, alsaLib, libpulseaudio, protobuf
-, xorg, libXtst, libXcomposite, libXdamage, libXext, libXrender, libXrandr
-, glib, abseil-cpp, pcre, util-linuxMinimal, libselinux, libsepol, pipewire
+, xorg, libXtst
 }:
 
-stdenv.mkDerivation {
+let
+  rev = "2d804d2c9c5d05324c8ab22f2e6ff8306521b3c3";
+  sha256 = "0kz0i381iwsgcc3yzsq7njx3gkqja4bb9fsgc24vhg0md540qhyn";
+
+in stdenv.mkDerivation {
   pname = "tg_owt";
-  version = "unstable-2021-06-17";
+  version = "git-${rev}";
 
   src = fetchFromGitHub {
     owner = "desktop-app";
     repo = "tg_owt";
-    rev = "f03ef05abf665437649a4f71886db1343590e862";
-    sha256 = "0s6ajw52b95lcq4mn6lv8gj6lhv62bvwjh43w7az2k5pbm14v7vv";
+    inherit rev sha256;
     fetchSubmodules = true;
   };
-
-  patches = [
-    # Our libXtst seems broken:
-    # /nix/store/rd3swxwmzjgjvwhz8svyc8ghc0brq293-libXtst-1.2.3/include/X11/extensions/XTest.h:32:10: fatal error: X11/extensions/XInput.h: No such file or directory
-    (fetchpatch {
-      # Copy updated source files.
-      url = "https://github.com/desktop-app/tg_owt/commit/2c0fbe4d3d1c33d0cc9ff7c112b4db1963bea535.patch";
-      sha256 = "0apd6hfv3a1s3qy10kjwk4z8bg835cpk0ql9qxjnxf4gq183bhif";
-      revert = true;
-      includes = [ "src/modules/desktop_capture/linux/shared_x_display.cc" ];
-    })
-  ];
 
   outputs = [ "out" "dev" ];
 
@@ -35,8 +24,7 @@ stdenv.mkDerivation {
 
   buildInputs = [
     libjpeg openssl libopus ffmpeg alsaLib libpulseaudio protobuf
-    xorg.libX11 libXtst libXcomposite libXdamage libXext libXrender libXrandr
-    glib abseil-cpp pcre util-linuxMinimal libselinux libsepol pipewire
+    xorg.libX11 libXtst
   ];
 
   cmakeFlags = [
