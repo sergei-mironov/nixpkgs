cac85e4a73f76b67ba15f5133b650cea2b56adea
Sergei Mironov <sergei.v.mironov@proton.me>
2024-06-26 20:28:57 +0400

    Update librem-ec-acpi-dkms: 0.9.1 -> 0.9.2

--- pkgs/os-specific/linux/purism/librem-ec-acpi-dkms/default.nix
+++ pkgs/os-specific/linux/purism/librem-ec-acpi-dkms/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "librem-ec-acpi-dkms";
-  version = "0.9.1-${kernel.version}";
+  version = "0.9.2-${kernel.version}";
 
   src = fetchFromGitLab {
     domain = "source.puri.sm";
     owner = "nicole.faerber";
     repo = "librem-ec-acpi-dkms";
-    rev = "9be1cc47fbe245f84adb5979c61ce74ae7f68b83";
-    sha256 = "1qnbfj60i8nn2ahgj2zp5ixd79bb0wl1ld36x3igws2f3c0f5pfi";
+    rev = "cec8c0e8bf1532c9c605f60bb02a2ef0f98e5d77";
+    sha256 = "sha256-gPb1/4UPbwjJiqtP27KKjiIKxIYEl2nlQACUAlxVEFU=";
   };
 
   hardeningDisable = [ "pic" ];
